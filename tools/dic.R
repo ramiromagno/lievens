e3905d7d44bced5dbf9aa59621ab3fef9f0535e3
Ramiro Magno <rmagno@pattern.institute>
2024-05-02 22:53:55 +0100

    Whitelist words

---
+++ tools/dic.R
@@ -0,0 +1,14 @@
+words <-
+  c(
+    "Lectin",
+    "Lievens",
+    "et",
+    "al",
+    "endogene",
+    "isopropanol",
+    "polymerase",
+    "qPCR",
+    "PCR",
+    "tannic"
+  )
+saveRDS(words, file = ".aspell/custom_dic.rds", version = 2)
